476a2d09eeefef03d4448c375fe18f3d4da83d90
xinyi0526 <xinyi.luvand620@gmail.com>
2020-08-06 13:11:04 +0800

    Submission

--- app/src/main/java/com/myapplicationdev/android/demonavigation/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demonavigation/MainActivity.java
@@ -2,13 +2,31 @@ package com.myapplicationdev.android.demonavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNewActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNewActivity = findViewById(R.id.btnNewActivity);
+
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/myapplicationdev/android/demonavigation/NewActivity.java
@@ -0,0 +1,20 @@
+package com.myapplicationdev.android.demonavigation;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
